7a72d2490d24ba4aaba6a2dfdddb5ebcc424e954
simo <mohammedlaamarti53@gmail.com>
2025-05-12 18:59:35 +0100

    modify

--- src/main/java/com/myapp/qrcode/mapper/SignatureMapper.java
+++ src/main/java/com/myapp/qrcode/mapper/SignatureMapper.java
@@ -8,17 +8,15 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 
-import java.time.LocalDate;
+
 
 @Mapper
 public interface SignatureMapper {
 
     SignatureMapper INSTANCE = Mappers.getMapper(SignatureMapper.class);
 
-    // Mapping vers DTO de réponse
     SignatureResponse toSignatureResponse(SignatureData data);
 
-    // Mapping depuis le DTO de création (CreateSignatureRequest)
     @Mapping(target = "dateCreation", expression = "java(LocalDate.now())")
     @Mapping(target = "dateExpiration", expression = "java(LocalDate.now().plusMonths(6))")
     SignatureData toSignatureData(CreateSignatureRequest request);
